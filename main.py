f87170307496b6413cb66cfc9a71ebfe6e6ed9c4
Jason Roman <funjasonroman@gmail.com>
2022-04-12 17:36:44 -0600

    Blocks generate as you walk around

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -7,16 +8,44 @@ BOARD = [{"x": x, "y": y, "state": 1 * ((x != 0) and (y != 0))} for x in [-1, 0,
 playerpos = [0, 0]
 SCREENSIZE = [500, 500]
 
+def getBlock(x: int, y: int) -> "dict[str, int] | None":
+	"""Returns the block at the given coordinates."""
+	for block in BOARD:
+		if block["x"] == x and block["y"] == y:
+			return block
+
+def getPlayerBlock() -> "dict[str, int]":
+	"""Returns the block the player is on."""
+	return getBlock(playerpos[0], playerpos[1])
+
 def playerMove(direction: str) -> None:
 	"""Moves the player in the given direction."""
 	if direction == "up":
 		playerpos[1] -= 1
+		if getPlayerBlock()["state"] == 1: playerpos[1] += 1
 	elif direction == "down":
 		playerpos[1] += 1
+		if getPlayerBlock()["state"] == 1: playerpos[1] -= 1
 	elif direction == "left":
 		playerpos[0] -= 1
+		if getPlayerBlock()["state"] == 1: playerpos[0] += 1
 	elif direction == "right":
 		playerpos[0] += 1
+		if getPlayerBlock()["state"] == 1: playerpos[0] -= 1
+	# Check the surrounding blocks to make sure they exist.
+	if getBlock(playerpos[0]    , playerpos[1]    ) == None: addBlock(playerpos[0]    , playerpos[1]    ) # Player position
+	if getBlock(playerpos[0] + 1, playerpos[1]    ) == None: addBlock(playerpos[0] + 1, playerpos[1]    ) # Right
+	if getBlock(playerpos[0]    , playerpos[1] + 1) == None: addBlock(playerpos[0]    , playerpos[1] + 1) # Down
+	if getBlock(playerpos[0] - 1, playerpos[1]    ) == None: addBlock(playerpos[0] - 1, playerpos[1]    ) # Left
+	if getBlock(playerpos[0]    , playerpos[1] - 1) == None: addBlock(playerpos[0]    , playerpos[1] - 1) # Up
+
+def addBlock(x: int, y: int) -> None:
+	"""Adds a block to the board."""
+	BOARD.append({"x": x, "y": y, "state": newBlockState()})
+
+def newBlockState() -> int:
+	"""Generates a new block state."""
+	return random.choices([0, 1], weights=[5, 1], k=1)[0]
 
 screen = pygame.display.set_mode(SCREENSIZE, pygame.RESIZABLE)
 
