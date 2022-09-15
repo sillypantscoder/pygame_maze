2a6e80360956b836688fca2446efbb26be666915
Jason Roman <funjasonroman@gmail.com>
2022-09-14 19:31:39 -0600

    You can now remember places you've been

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Cell:
 	def __init__(self, pos, state):
 		self.pos = pos
 		self.state = state
-		self.light = 0
+		self.light = 0 # 0 = never seen; 1 = seen before; 2 = seeing now
 	def getColor(self) -> tuple:
 		if self.state == "wall":
 			return (255, 255, 255)
@@ -27,9 +27,9 @@ class Cell:
 		points = get_line(self.pos, playerpos)
 		for point in points:
 			if not BOARD[point[0]][point[1]].canwalk():
-				self.light = 0
+				self.light = 0 if self.light == 0 else 1
 				return
-		self.light = 1
+		self.light = 2
 
 cellsize = 10
 boardsize = [*boardgen.boardsize]
@@ -46,13 +46,13 @@ pygame.key.set_repeat(500, 10)
 def PLAYERTHREAD():
 	global playerpos
 	global running
+	needslightrefresh = [-1]
 	def playerMove(newX, newY):
 		global playerpos
 		if BOARD[newX][newY].canwalk():
 			playerpos = [newX, newY]
-			for i in range(boardsize[0]):
-				for j in range(boardsize[1]):
-					BOARD[i][j].refreshLight()
+			if needslightrefresh[0] == -1:
+				needslightrefresh[0] = 10 # Darn references
 	c = pygame.time.Clock()
 	while running:
 		if inputkey == pygame.K_UP:
@@ -63,6 +63,15 @@ def PLAYERTHREAD():
 			playerMove(playerpos[0] - 1, playerpos[1])
 		elif inputkey == pygame.K_RIGHT:
 			playerMove(playerpos[0] + 1, playerpos[1])
+		# Light refresh
+		if needslightrefresh[0] >= 0:
+			needslightrefresh[0] -= 1
+		if needslightrefresh[0] == 0:
+			def lightrefresh():
+				for i in range(boardsize[0]):
+					for j in range(boardsize[1]):
+						BOARD[i][j].refreshLight()
+			threading.Thread(target=lightrefresh).start()
 		c.tick(60)
 
 def MAIN():
@@ -87,6 +96,11 @@ def MAIN():
 		for i in range(boardsize[0]):
 			for j in range(boardsize[1]):
 				if BOARD[i][j].light == 1:
+					overlay = pygame.Surface((cellsize, cellsize))
+					overlay.set_alpha(100)
+					overlay.fill(BOARD[i][j].getColor())
+					screen.blit(overlay, (i * cellsize, j * cellsize))
+				if BOARD[i][j].light == 2:
 					pygame.draw.rect(screen, BOARD[i][j].getColor(), pygame.Rect(i * cellsize, j * cellsize, cellsize, cellsize))
 		# Player
 		pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(playerpos[0] * cellsize, playerpos[1] * cellsize, cellsize, cellsize))
